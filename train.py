748a4cd6eb50aba319718e1b6708349c6045e3b3
smmitchell2 <smmitchell3@gmail.com>
2018-03-01 10:23:03 -0600

    figuring out what functions I need

--- train.py
+++ train.py
@@ -1,2 +1,23 @@
-from tensorflow import tf
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import collections
+import math
+import os
+import random
+
+import numpy as np
+from six.moves import urllib
+from six.moves import xrange  # pylint: disable=redefined-builtin
+import tensorflow as tf
 from sklearn import svm
+
+def read_data(filename):
+    return data
+
+def build_dataset(words,n_words):
+    return data,count,dictionary,reverse_dictionary
+
+def train():
+    
